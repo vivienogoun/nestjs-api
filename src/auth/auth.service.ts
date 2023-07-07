942941943a84e82615fbd7749533ee9aee3bd607
Vivien Ogoun <vivienogoun1@gmail.com>
2023-07-07 20:06:08 +0100

    Set up the project, add modules, set up postgres in docker, set up prisma and create models ;)

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService {
+
+    signup() {
+        return {msg: 'signup'};
+    }
+
+    signin() {
+        return {msg: 'signin'};
+    }
+}
